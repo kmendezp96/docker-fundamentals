a7b37dc1109c74873e73dcfe3a52a4f8af8f6a9c
manherfo <35644952+manherfo@users.noreply.github.com>
2018-07-27 16:09:07 -0500

    docker-compose installation

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -7,5 +7,7 @@ sudo apt-get update  > /dev/null 2>&1
 echo "Installing Node"
 sudo apt-get install -y nodejs > /dev/null 2>&1
 sudo apt-get install -y npm > /dev/null 2>&1
-
+echo "Installing docker-compose"
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose /dev/null 2>&1
+sudo chmod +x /usr/local/bin/docker-compose
 echo "Success"
